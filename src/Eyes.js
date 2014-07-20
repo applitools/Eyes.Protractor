b07b4ea9a6499245f1cc71d3943fa676e17ab1a7
applitools <team@applitools.com>
2014-07-20 10:28:12 +0300

    Created Protractor package

---
+++ src/Eyes.js
@@ -0,0 +1,141 @@
+/*
+ ---
+
+ name: Eyes
+
+ description: The main type - to be used by the users of the library to access all functionality.
+
+ provides: [Eyes]
+ requires: [ServerConnector, EyesBase]
+
+ ---
+ */
+
+;(function() {
+    "use strict";
+
+    var EyesBase = require('./EyesBase'),
+        EyesWebDriver = require('./EyesWebDriver'),
+        ViewportSize = require('./ViewportSize'),
+        PromiseFactory = require('EyesPromiseFactory.js'),
+        webdriver = require('selenium-webdriver');
+
+    /**
+     *
+     * C'tor = initializes the module settings
+     *
+     * @param {String} serverUrl
+     * @param {Number} matchTimeout
+     * @param {Boolean} isDisabled - set to true to disable Applitools Eyes and use the webdriver directly.
+     *
+     **/
+    function Eyes(serverUrl, matchTimeout, isDisabled) {
+        PromiseFactory.setPromiseHandler(function (asyncAction) {
+            return webdriver.promise.controlFlow().execute(function () {
+                var deferred = webdriver.promise.defer();
+                asyncAction(deferred);
+                return deferred.promise;
+            });
+        });
+        EyesBase.call(this, serverUrl || EyesBase.DEFAULT_EYES_SERVER, matchTimeout || 2000, isDisabled);
+    }
+
+    Eyes.prototype = new EyesBase();
+    Eyes.prototype.constructor = Eyes;
+
+    EyesBase.agentId = 'javascript/0.0';
+
+    // TODO: instance method
+    Eyes.setApiKey = function (apiKey) {
+        EyesBase.apiKey = apiKey;
+    };
+
+    Eyes.prototype.open = function (driver, appName, testName, viewportSize, matchLevel, failureReports) {
+        var flow = webdriver.promise.controlFlow();
+        return flow.execute(function () {
+            var deferred = webdriver.promise.defer();
+            console.log('execution began for eyes open');
+            try {
+                EyesBase.prototype.open.call(this, appName, testName, viewportSize, matchLevel, failureReports)
+                    .then(function () {
+                        console.log('inner eyes open returned - fulfilling');
+                        this._driver = driver; //TODO: new EyesWebDriver(driver, this);
+                        // this._driver.init().then(function () {
+                        deferred.fulfill(this._driver);
+                        //}.bind(this));
+                    }.bind(this));
+            } catch (err) {
+                console.log(err);
+                deferred.reject(err);
+            }
+
+            return deferred.promise;
+        }.bind(this));
+    };
+
+    Eyes.prototype.close = function (throwEx) {
+        var flow = webdriver.promise.controlFlow();
+        return flow.execute(function () {
+            var deferred = webdriver.promise.defer();
+            console.log('execution began for eyes close');
+            try {
+                EyesBase.prototype.close.call(this, throwEx)
+                    .then(function () {
+                        console.log('inner eyes close returned - fulfilling');
+                        deferred.fulfill(this._driver);
+                    }.bind(this));
+            } catch (err) {
+                console.log(err);
+                deferred.reject(err);
+            }
+
+            return deferred.promise;
+        }.bind(this));
+    };
+
+    Eyes.prototype.checkWindow = function (tag) {
+        var flow = webdriver.promise.controlFlow();
+        return flow.execute(function () {
+            var deferred = webdriver.promise.defer();
+            console.log('execution began for eyes check window');
+            try {
+                EyesBase.prototype.checkWindow.call(this, tag, false).then(function () {
+                        console.log('inner eyes check window returned - fulfilling');
+                        deferred.fulfill();
+                    }.bind(this),
+                    function (err) {
+                        console.log(err);
+                        deferred.reject(err);
+                    });
+            } catch (err) {
+                console.log(err);
+                deferred.reject(err);
+            }
+
+            console.log('returning check window promise');
+            return deferred.promise;
+        }.bind(this));
+    };
+
+    Eyes.prototype._waitTimeout = function (ms) {
+        return webdriver.promise.controlFlow().timeout(ms);
+    };
+
+    Eyes.prototype.getScreenshot = function () {
+        return this._driver.takeScreenshot();
+    };
+
+    Eyes.prototype.getTitle = function () {
+        return this._driver.getTitle();
+    };
+
+    Eyes.prototype.getViewportSize = function () {
+        return ViewportSize.getViewportSize(this._driver);
+    };
+
+    Eyes.prototype.setViewportSize = function (size) {
+        return ViewportSize.setViewportSize(this._driver, size);
+    };
+
+    module.exports = Eyes;
+}());
---
+++ src/ViewportSize.js
@@ -0,0 +1,86 @@
+/*
+ ---
+
+ name: ViewportSize
+
+ description: helper method to acquire viewport dimensions.
+
+ provides: [ViewportSize]
+
+ ---
+ */
+
+;(function() {
+    "use strict";
+
+    var PromiseFactory = require('./../../Eyes.Sdk.JavaScript/src/EyesPromiseFactory'),
+        webdriver = require('selenium-webdriver');
+    var Window = webdriver.WebDriver.Window;
+
+    // consts
+    var _GET_VIEWPORT_SIZE_JAVASCRIPT_FOR_NORMAL_BROWSER =
+            'return {width: window.innerWidth, height: window.innerHeight}',
+        _GET_VIEWPORT_SIZE_JAVASCRIPT_FOR_BAD_BROWSERS =
+            'return {width: document.documentElement.clientWidth, height: document.documentElement.clientHeight}';
+
+    var ViewportSize = {};
+
+    /**
+     *
+     * Tries to get the viewport size using Javascript. If fails, gets the entire browser window size!
+     *
+     * @method getViewportSize
+     * @param {Object} driver
+     *
+     * @return {Object} the size
+     *
+     **/
+    ViewportSize.getViewportSize = function (driver) {
+        console.log('ViewportSize.getViewportSize called');
+        return PromiseFactory.makePromise(function (deferred) {
+            try {
+                console.log('ViewportSize.getViewportSize - executing scripts');
+                driver.executeScript(_GET_VIEWPORT_SIZE_JAVASCRIPT_FOR_NORMAL_BROWSER).then(function(size) {
+                        if (size.width > 0 && size.height > 0) {
+                            console.log('ViewportSize.getViewportSize - normal script returned size: w', size.width,
+                            ' h', size.height);
+                            deferred.fulfill(size);
+                            return;
+                        }
+
+                        console.log('ViewportSize.getViewportSize - normal script returned bad size - calling old script');
+
+                        driver.executeScript(_GET_VIEWPORT_SIZE_JAVASCRIPT_FOR_BAD_BROWSERS).then(function(size) {
+                            console.log('ViewportSize.getViewportSize - old script returned size: w', size.width,
+                                ' h', size.height);
+                            deferred.fulfill(size);
+                        });
+                    },
+                    function(err){
+                        console.log('ViewportSize.getViewportSize - error while getting viewport size - ' +
+                            'attempting old browser script', err);
+                        driver.executeScript(_GET_VIEWPORT_SIZE_JAVASCRIPT_FOR_BAD_BROWSERS).then(function(size) {
+                            console.log('ViewportSize.getViewportSize - old script returned size: w', size.width,
+                                ' h', size.height);
+                            deferred.fulfill(size);
+                        });
+                    });
+            } catch (err) {
+                console.log('ViewportSize.getViewportSize - exception - so using window size', err);
+                new Window(driver).getSize().then(function(size){
+                    console.log('ViewportSize.getViewportSize - window returned size: w', size.width,
+                        ' h', size.height);
+                    deferred.fulfill(size);
+                });
+            }
+        }.bind(this));
+    };
+
+    // TODO: Implement setViewPortSize
+    ViewportSize.setViewportSize = function (driver, size) {
+        console.log('ViewportSize.setViewportSize - not implemented');
+        throw "TODO: Implement setViewPortSize";
+    };
+
+    module.exports = ViewportSize;
+}());
