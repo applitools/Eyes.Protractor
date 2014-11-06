91df7479fbac2075cd6782fc760bf256fe5c5ab3
Daniel Puterman <daniel.puterman@applitools.com>
2014-11-06 12:57:10 +0200

    agentId update + Rev up

--- src/Eyes.js
+++ src/Eyes.js
@@ -41,7 +41,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'eyes-protractor/0.0.22';
+        return 'eyes-protractor/0.0.24';
     };
 
     function _init(that, flow) {
