1c11003ccc42746033963c95f67f52341f239bba
Doron Zavelevsky <zavelevsky@gmail.com>
2016-03-26 22:39:15 +0300

    fixes to support newer selenium version
    Changed access to capabilities to be compatible with >2.4.8 versions
    (still supporting older versions).
    Also required a fix to utils mixing

--- src/Eyes.js
+++ src/Eyes.js
@@ -96,10 +96,17 @@
     return flow.execute(function() {
       return that._driver.getCapabilities()
         .then(function(capabilities) {
-          var platformName = capabilities.caps_.platformName;
-          var platformVersion = capabilities.caps_.platformVersion;
-          var orientation = capabilities.caps_.orientation || capabilities.caps_.deviceOrientation;
-
+          var platformName, platformVersion, orientation;
+          if (capabilities.caps_) {
+            platformName = capabilities.caps_.platformName;
+            platformVersion = capabilities.caps_.platformVersion;
+            orientation = capabilities.caps_.orientation || capabilities.caps_.deviceOrientation;
+          } else {
+            platformName = capabilities.get('platform');
+            platformVersion = capabilities.get('version');
+            orientation = capabilities.get('orientation') || capabilities.get('deviceOrientation');
+          }
+          
           var majorVersion;
           if (!platformVersion || platformVersion.length < 1) {
             return;
